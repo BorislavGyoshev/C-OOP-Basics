a1c7f3bd7e4480f65724ad339e1c6d9425e1919d
BorislavGyoshev <b.gioshev@abv.bg>
2017-07-17 14:47:22 +0300

    Add files via upload

---
+++ EncapsulationLab/SortPersonsByNameAndAge/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Person
+{
+    private string firtsName;
+    private string lastName;
+    private int age;
+
+    public Person(string firtsName, string lastName, int age)
+    {
+        this.firtsName = firtsName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public string FirstName { get { return this.firtsName; } }
+
+    public int Age { get { return this.age; } }
+
+    public override string ToString()
+    {
+        return $"{this.firtsName} {this.lastName} is a {this.age} years old.";
+    }
+}
